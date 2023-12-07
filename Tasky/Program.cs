980b2243da52dd2d2a7d7660e6ed398df0832fc5
Roby <racsente@gmail.com>
2023-12-07 13:08:18 +0200

    test?

--- Tasky/Program.cs
+++ Tasky/Program.cs
@@ -4,7 +4,7 @@ using Tasky.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the this container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 	options.UseSqlServer(connectionString));
