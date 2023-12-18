c8aa5267fac9ba809dd03f6e8dbd3f8e708e89b1
Roby <racsente@gmail.com>
2023-12-18 08:50:33 +0200

    added comment validation

--- Tasky/wwwroot/js/site.js
+++ Tasky/wwwroot/js/site.js
@@ -8,35 +8,55 @@
             placeholderElement.find('.modal').modal('show');
         });
     });
+    $(function () {
 
-    placeholderElement.on('click', '[data-save="modal"]', function (event) {
-        event.preventDefault();
-        //post form data
-  /*      console.log(event)
-        console.log(event.target.id)*/
-        // Get the ID of the clicked button
-        var buttonId = event.target.id;
-        //use the button id to select the correct form
-        var formClass = '.modal-body form#' + buttonId;
-        var form = $(formClass);
 
-        var actionUrl = form.attr('action');
-        var dataToSend = form.serialize();
-        console.log(dataToSend)
-        $.post(actionUrl, dataToSend).done(function (data) {
-            var newForm = $(data).find(formClass);
+        placeholderElement.on('click', '[data-save="modal"]', function (event) {
+            event.preventDefault();
+            //post form data
 
-            // Replace the content of the existing form
-            form.replaceWith(newForm);
-            // find IsValid input field and check it's value
-            // if it's valid then hide modal window
-            var isValid = newBody.find('[name="IsValid"]').val() == 'True';
-            if (isValid) {
-                placeholderElement.find('.modal').modal('hide');
-                location.reload();
-            }
+            // Get the ID of the clicked button
+            var buttonId = event.target.id;
+            //use the button id to select the correct form
+            if (buttonId == 'form_add' || buttonId=='test_id')
+                var formClass = '.modal-body form#' + buttonId;
+            else
+                var formClass = '.modal-footer form#' + buttonId;
+
+            var form = $(formClass);
+
+            var actionUrl = form.attr('action');
+            var dataToSend = form.serialize();
+            console.log(dataToSend);
+            $.post(actionUrl, dataToSend).done(function (data) {
+                var newForm = $(data).find(formClass);
+                // Replace the content of the existing form
+                form.replaceWith(newForm);
+                // find IsValid input field and check it's value
+                // if it's valid then hide modal window
+                var isValid = newForm.find('[name="IsValid"]').val() == 'True';
+
+                if (isValid) {
+                    if (buttonId == 'test_id') {//taskModal
+                        placeholderElement.find('.modal').modal('hide');
+                        location.reload();
+                    }
+                    else {//taskShowModal
+
+                        var messageContainer = placeholderElement.find('#commentMessage');
+                        messageContainer.text('Comment saved successfully');
+                        //send get request to show the newly created comment
+                        var newFooter = $(data).find('.modal-footer');
+                        var footer = $('.modal-footer')
+                        footer.replaceWith(newFooter)
+                        setTimeout(function () {
+                            messageContainer.text(''); // Clear the message after a few seconds
+                        }, 3000); // Adjust the duration as needed
+                    }
+                }
+
+            });
+
         });
-
-    });
-  
-});
+    })
+})
